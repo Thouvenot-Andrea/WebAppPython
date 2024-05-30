2b79ba5bb4333cab2b5b55331365d34f6155fadd
thouvenot <thouvenot@thouvenot-desktop>
2024-05-30 11:50:10 +0200

    Commit suivant

--- bottle_app.py
+++ bottle_app.py
@@ -4,7 +4,7 @@ from bottle import default_app, route
 
 @route('/')
 def hello_world():
-    return 'Hello from Python'
+    return 'Hello from Python1'
 
 
 application = default_app()
---
+++ test_bottle_app.py
@@ -0,0 +1,5 @@
+import bottle_app
+
+
+def test_hello_world():
+    assert bottle_app.hello_world() == 'Hello from Python1'
